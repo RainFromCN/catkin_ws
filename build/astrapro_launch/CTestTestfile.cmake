485bd5de07c1ea6aa6c9975be88587b743240204
rain_ <RainFromCN@gmail.com>
2020-11-01 23:47:07 +0800

    update

--- build/astrapro_launch/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lwh/catkin_ws/src/astrapro_launch
-# Build directory: /home/lwh/catkin_ws/build/astrapro_launch
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/hardware_init/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lwh/catkin_ws/src/hardware_init
-# Build directory: /home/lwh/catkin_ws/build/hardware_init
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
